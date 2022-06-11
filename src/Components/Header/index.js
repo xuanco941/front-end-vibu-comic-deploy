7895238f9773acbc639fd373643fbe204a0f64d8
xuanco941 <xuanco941@gmail.com>
2022-06-11 16:34:36 +0700

    change location header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,10 +3,11 @@ import style from './header.module.css';
 
 
 const Header = () => {
+    let location = window.location.href;
     return (
         <>
             <header className={clsx(style.header)}>
-                <a href='/'>
+                <a href={location}>
                     <h1>Vibu Comics</h1>
                 </a>
             </header>
